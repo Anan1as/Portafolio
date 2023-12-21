0b6022d1dbeac70d7a6cf9d2d848eb45c6740bfe
Ananias <amychelleoropezaortiz@gmail.com>
2023-12-20 20:25:37 -0500

    Inicio del Javascript y construcción del CSS para mejor vision

---
+++ JS/script.js
@@ -0,0 +1,30 @@
+//Linea 1
+
+let btnMacondo = document.getElementById("btnMacondo");
+let btnAhorcado = document.getElementById("btnAhorcado");
+let btnHogwarts = document.getElementById("btnHogwarts");
+let btnRegistroEs = document.getElementById("btnRegistroEs");
+
+btnMacondo.addEventListener("click", function() {
+    location.href = "https://github.com/Anan1as/Macondo"
+});
+
+btnAhorcado.addEventListener("click", function() {
+    location.href = "https://github.com/Anan1as/Juego-Ahorcado---Review"
+});
+
+btnHogwarts.addEventListener("click", function() {
+    location.href = "https://github.com/Anan1as/Hogwarts"
+});
+
+btnRegistroEs.addEventListener("click", function() {
+    location.href = "https://github.com/Anan1as/RegistroEstudiantes"
+});
+
+
+//Linea 2
+let btnTricky = document.getElementById("btnTricky");
+
+btnTricky.addEventListener("click", function() {
+    location.href = "https://github.com/Anan1as/Triky"
+});
