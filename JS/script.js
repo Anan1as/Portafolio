9ce04c333d91392f4fdd13eeaa96dc45fae75bd8
Ananias <amychelleoropezaortiz@gmail.com>
2024-01-09 15:41:58 -0500

    Agregados los proyectos, "KFC" e "Inicio de Sesion" junto con sus fotos y links, también se ha agregado una nueva fuente y el link para ir al perfil de Github.

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,4 @@
 //Linea 1
-
 let btnMacondo = document.getElementById("btnMacondo");
 let btnAhorcado = document.getElementById("btnAhorcado");
 let btnHogwarts = document.getElementById("btnHogwarts");
@@ -24,7 +23,17 @@ btnRegistroEs.addEventListener("click", function() {
 
 //Linea 2
 let btnTricky = document.getElementById("btnTricky");
+let btnIniciodeSesion = document.getElementById("btnIniciodeSesion");
+let btnKFC = document.getElementById("btnKFC");
 
 btnTricky.addEventListener("click", function() {
     location.href = "https://anan1as.github.io/Triky/"
 });
+
+btnIniciodeSesion.addEventListener("click", function() {
+    location.href = "https://anan1as.github.io/Inicio-de-sesion/"
+});
+
+btnKFC.addEventListener("click", function () {
+    location.href = "https://anan1as.github.io/burga/"
+})
